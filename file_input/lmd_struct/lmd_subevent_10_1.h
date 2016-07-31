4840f26ba36ca61718bd09ceac45d700bcbfd503
HTJ <f96hajo@chalmers.se>
2016-07-31 19:08:47 +0200

    Syncronising lmd_subevent_10_1 header with drasi.  -> uint32_t

--- file_input/lmd_struct/lmd_subevent_10_1.h
+++ file_input/lmd_struct/lmd_subevent_10_1.h
@@ -18,48 +18,35 @@
  * MA  02110-1301  USA
  */
 
-#ifndef __LMD_SUBEVENT_10_1_H__
-#define __LMD_SUBEVENT_10_1_H__
+#ifndef __LMD_STRUCT__LMD_SUBEVENT_10_1_H__
+#define __LMD_STRUCT__LMD_SUBEVENT_10_1_H__
 
 #include "lmd_event_10_1.h"
 
-/*  ================= GSI VME Subevent header =======================  */
+/********************************************************************/
+/* GSI VME subevent header */
 
 typedef struct
 {
   lmd_event_header_little_endian _header;
 
-  sint16  i_procid;     /*  Processor ID [as loaded from VAX] */
-  sint8   h_subcrate;   /*  Subcrate number */
-  sint8   h_control;    /*  Processor type code */
+  uint16_t  i_procid;     /* Processor ID [as loaded from VAX] */
+  uint8_t   h_subcrate;   /* Subcrate number */
+  uint8_t   h_control;    /* Processor type code */
 } lmd_subevent_10_1_little_endian;
 
 typedef struct
 {
   lmd_event_header_big_endian _header;
 
-  sint8   h_control;    /*  Processor type code */
-  sint8   h_subcrate;   /*  Subcrate number */
-  sint16  i_procid;     /*  Processor ID [as loaded from VAX] */
+  uint8_t   h_control;    /* Processor type code */
+  uint8_t   h_subcrate;   /* Subcrate number */
+  uint16_t  i_procid;     /* Processor ID [as loaded from VAX] */
 } lmd_subevent_10_1_big_endian;
 
 typedef lmd_subevent_10_1_big_endian        lmd_subevent_10_1_network_order;
 #define lmd_subevent_10_1_host              HOST_ENDIAN_TYPE(lmd_subevent_10_1)
 
-/* ------------------------------------------------------------------ */
+/********************************************************************/
 
-typedef struct
-{
-  lmd_event_header_network_order _header;
-
-  // these are not used for anything,
-  // so avoid them in repacked data
-
-  // sint8   h_control;   /*  Processor type code */
-  // sint8   h_subcrate;   /*  Subcrate number */
-  // sint16 i_procid;   /*  Processor ID [as loaded from VAX] */
-} lmd_subevent_10_1_x_network_order;
-
-/* ------------------------------------------------------------------ */
-
-#endif// __LMD_SUBEVENT_10_1_H__
+#endif/*__LMD_STRUCT__LMD_SUBEVENT_10_1_H__*/
