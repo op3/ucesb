7be367c9c16abbc79448a518f29c986501511d5c
Haakan T Johansson <f96hajo@chalmers.se>
2018-08-11 20:46:32 +0200

    Split the testing into functions for setup and event packing.

--- hbook/example/ext_writer_test.cc
+++ hbook/example/ext_writer_test.cc
@@ -44,23 +44,14 @@ mystruct event;
 
 /* The code for the below can be obtained from:
  *
- * ../make_external_struct_sender.pl --struct=mystruct < external_writer_test.cc
+ * ../make_external_struct_sender.pl --struct=mystruct < ext_writer_test.cc
  *
  * Which even better would be invoked in the build process.
  * But not in this example.
  */
 
-int main(int argc, char *argv[])
+void send_offsets_mystruct(external_writer *ew)
 {
-  main_argv0 = argv[0];
-
-  ew = new external_writer();
-
-  ew->init(NTUPLE_TYPE_ROOT | NTUPLE_CASE_KEEP, false,
-	   "monsterfile.root","Title",-1,false,false,false,false);
-
-  ew->send_file_open();
-
   ew->send_book_ntuple(101,"h101","CoolTree");
 
   ew->send_alloc_array(sizeof(mystruct));
@@ -71,13 +62,13 @@ int main(int argc, char *argv[])
 			 "c",-1,"",EXTERNAL_WRITER_FLAG_TYPE_FLOAT32);
   ew->send_hbname_branch("DEF",offsetof(mystruct,b),sizeof(event.b),
 			 "b",-1,"",EXTERNAL_WRITER_FLAG_TYPE_INT32);
-  ew->send_hbname_branch("DEF",offsetof(mystruct,d),sizeof(event.d),
+  ew->send_hbname_branch("DEF",offsetof(mystruct,d),sizeof(event.d[0]),
 			 "d",4,"b",EXTERNAL_WRITER_FLAG_TYPE_FLOAT32);
-  ew->send_hbname_branch("DEF",offsetof(mystruct,e),sizeof(event.e),
+  ew->send_hbname_branch("DEF",offsetof(mystruct,e),sizeof(event.e[0]),
 			 "e",7,"",EXTERNAL_WRITER_FLAG_TYPE_FLOAT32);
 
   uint32_t offset_msg_size = (1+1+1+(2)+4+7) * (uint32_t) sizeof(uint32_t);
-  uint32_t fill_msg_size = (2 + (1+1+1+4+7)) * (uint32_t) sizeof(uint32_t);
+  uint32_t fill_msg_size = (1 + (1+1+1+4+7)) * (uint32_t) sizeof(uint32_t);
 
   uint32_t maxmsgsize =
     fill_msg_size > offset_msg_size ?
@@ -100,42 +91,79 @@ int main(int argc, char *argv[])
       *(o++) = htonl((uint32_t) ((offsetof(mystruct,e)+i*sizeof(float))));
     ew->send_offsets_fill(o);
   }
+}
+
+void send_fill_mystruct(external_writer *ew,
+                       const mystruct &s,
+                       uint32_t ntuple_index = 0)
+{
+  uint32_t fill_msg_size = (1 + (1+1+1+4+7)) * (uint32_t) sizeof(uint32_t);
+
+  uint32_t *p = ew->prepare_send_fill(fill_msg_size, ntuple_index);
+
+  *(p++) = htonl(0x40000000); /* marker */
+
+  {
+    *(p++) = htonl((s.a));
+    *(p++) = htonl(external_write_float_as_uint32(s.c));
+    {
+      uint32_t loop_n_b = s.b;
+      assert (loop_n_b <= 4);
+      *(p++) = htonl(loop_n_b);
+      for (int l = 0; l < loop_n_b; l++) {
+	*(p++) = htonl(external_write_float_as_uint32(s.d[l]));
+      }
+    }
+    for (int i = 0; i < 7; i++)
+      *(p++) = htonl(external_write_float_as_uint32(s.e[i]));
+  }
+
+  ew->send_offsets_fill(p);
+}
+
+int main(int argc, char *argv[])
+{
+  main_argv0 = argv[0];
+
+  ew = new external_writer();
+
+  ew->init(NTUPLE_TYPE_ROOT | NTUPLE_CASE_KEEP, false,
+	   "monsterfile.root","Title",-1,false,false,false,false);
+
+  ew->send_file_open();
+
+  send_offsets_mystruct(ew);
 
   ew->send_setup_done();
 
   /* loop over events */
   for (int i = 0; i < 12345; i++)
     {
-      uint32_t *p = ew->prepare_send_fill(fill_msg_size);
-
-      *(p++) = htonl(0); /* marker */
-
       struct mystruct s;
 
       memset(&s, 0, sizeof (s));
 
-      {
-	*(p++) = htonl((s.a));
-	*(p++) = htonl(external_write_float_as_uint32(s.c));
-	{
-	  uint32_t loop_n_b = s.b;
-	  assert (loop_n_b <= 4);
-	  *(p++) = htonl(loop_n_b);
-	  for (int l = 0; l < loop_n_b; l++) {
-	    *(p++) = htonl(external_write_float_as_uint32(s.d[l]));
-	  }
-	}
-	for (int i = 0; i < 7; i++)
-	  *(p++) = htonl(external_write_float_as_uint32(s.e[i]));
-      }     
+      s.a = i;
+      s.c = (float) (i * 0.1);
+      s.b = i % 3;
+      s.e[0] = (float) (i * 0.01);
+      s.e[1] = (float) (i * 0.01 + 0.001);
 
-      ew->send_offsets_fill(p);
+      send_fill_mystruct(ew, s, 0);
     }
 
   ew->close(); // not required, done in destructor below
 
   delete ew;
+
+  return 0;
 }
+
+
+
+/* Code that would normally be elsewhere, but that we for simplicity
+ * just compile together.
+ */
 
 #define EXTERNAL_WRITER_NO_SHM 1
 
